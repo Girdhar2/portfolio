cd01f7cbac4db2f8e50015029cb6f0a4b6f76935
Girdhar Mundra <90412120+Girdhar2@users.noreply.github.com>
2023-10-03 01:25:42 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Satyam Shukla";
+            document.title = "Portfolio | Girdhar Mundra";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
